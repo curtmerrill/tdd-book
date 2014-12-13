553b56a9056a3a3fdaed71a1d99f0892f2c9056d
Curt Merrill <curt@curtmerrill.com>
2014-12-13 11:28:45 -0500

    First FT specs in comments, now using unittest library

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She
+        # goes to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "buy peacock feathers" into a text box (she
+        # likes to tie fly-fishing lures)
+
+        # When she hits enter, the page updates and the list
+        # includes her new to-do item
+
+        # A second text-box invites her to add another item to the
+        # list, so she enters "use feathers to make a fly"
+
+        # The page updates again, showing both items in the list
+
+        # Edith wonders if the site will remember her list. The site has
+        # generated what looks like a unique URL and there is some help
+        # text affirming that her list is safe.
+
+        # She visits the unique URL and sees her list
+
+        # Done. Sleep.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
